3f0e5aedce25b87c9f415134eb3a452434be3fe0
DCZ_Yewen <lzhallinone@outlook.com>
2021-09-12 13:28:54 +0800

    partly done file oprations wrapper

---
+++ README.md
@@ -0,0 +1,6 @@
+## RemoteDrive
+
+### Notice
+This program only compiles under C++17 or higher(invoking filesystem),
+if you wish it compiles under C++11 or older, please make your own 
+modifications.
---
+++ server.cpp
@@ -0,0 +1,27 @@
+#include "LightLOG/lightlog.h"
+#include "cmdline/cmdline.h"
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <filesystem>
+
+#include <hv/hsocket.h>
+#include <hv/hsysinfo.h>
+#include <hv/hthread.h>
+/*
+This library provides high performance aysc io
+and thread-safe socket facilities.
+*/
+
+
+#include "fileop.h"
+
+int main(int argc, char *argv[]){
+    cmdline::parser p_parser;
+
+
+    return 0;
+}
